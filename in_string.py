79a5a84c18e90d87ae7e321d53cc4c84c943d856
CaOttaviano <cmottaviano@mail.austral.edu.ar>
2025-04-07 12:34:05 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,10 @@
 def check_vowels():
+	nombre = input()
+	print("Contine a: " + str("a" in nombre))
+	print("Contine e: " + str("e" in nombre))
+	print("Contine i: " + str("i" in nombre))
+	print("Contine o: " + str("o" in nombre))
+	print("Contine u: " + str("u" in nombre))
     # Código a implementar utilizando input.
 
 
