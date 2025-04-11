a52840aa9196f7ea160dbe3250006a480e1783cd
CaOttaviano <cmottaviano@mail.austral.edu.ar>
2025-04-11 09:27:28 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,6 +1,6 @@
 def slice_advanced():
 	txt = input ()
-	print (str(txt)[3::2])
+	print (str(txt)[4::2])
     # Código a implementar utilizando input.
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
